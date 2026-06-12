46fdd63b7896e8b40aad21204bbef906d2824ae0
Abha Dongre <abhakdongre@gmail.com>
2026-06-12 11:20:10 +0530

    Removed dist from tracking

--- dist/script.d.ts
+++
@@ -1,7 +0,0 @@
-declare const chatBox: HTMLDivElement;
-declare const userInput: HTMLInputElement;
-declare const sendBtn: HTMLButtonElement;
-declare function addMessage(message: string, className: string): void;
-declare function showTyping(): HTMLDivElement;
-declare function getBotReply(message: string): Promise<string>;
-//# sourceMappingURL=script.d.ts.map
